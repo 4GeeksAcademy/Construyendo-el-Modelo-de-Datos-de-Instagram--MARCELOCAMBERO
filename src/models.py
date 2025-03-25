9e7227e42b0e9c86e772208624103442a5483039
Marcelo Cambero <camberotje@gmail.com>
2025-03-25 17:04:33 +0000

    ejercicio terminado, falta revisar los diagramas por no saber cuales son las extensiones que faltan

--- src/models.py
+++ src/models.py
@@ -11,10 +11,11 @@ class User(db.Model):
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True, nullable=False)
     firstname = Column(String(50), nullable=False)
-    secondname = Column(String(50), nullable=False)
-    email = Column(String(50), unique=True, nullable=False)
+    lastname = Column(String(50), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)3
 
     conection_comment = relationship("Comment", back_populates="conection_user")
+    conection_follower = relationship("Follower", "conection_user2")
 
     def serialize(self):
         return {
@@ -32,9 +33,10 @@ class Comment(db.Model):
     id = Column(Integer, primary_key=True)
     comment_text = Column(String(50), nullable=False)
     author_id = Column(Integer, ForeignKey("user.id"), nullable=False)
-    post_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id") nullable=False)
 
     conection_user = relationship("User", back_populates="conection_comment")
+    conection_post = relationship("Post", back_populates="conection_comment2")
 
     def serialize(self):
         return {
@@ -47,7 +49,10 @@ class Comment(db.Model):
 class Post (db.Model):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id") nullable=False)
+
+    conection_comment2 = relationship("Comment", "conection_post")
+    conection_media = relationship("Media", "conection_post2")
 
 def serialize(self):
         return {
@@ -59,7 +64,10 @@ class Media (db.Model):
     id = Column(Integer, primary_key=True)
     type = Column(Enum("image", name="media_types"))
     url = Column(String)
-    post_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, ForeignKey("post.id") nullable=False)
+
+    conection_post2 = relationship("Post", "conection_media")
+
 def serialize(self):
         
         return {
@@ -71,12 +79,15 @@ def serialize(self):
 
 class Follower (db.Model):
      __tablename__ = 'media'
-     user_from_id = Column(Integer, primary_key=True)
-     user_to_id = Column(Integer)
+     user_from_id = Column(Integer, ForeignKey("user.id") primary_key=True)
+     user_to_id = Column(Integer, ForeignKey("user.id"))
+
+     conection_user2 = relationship("User", "conection_follower")
+
 
 def serialize(self):
-     return {
-        user_from_id: self.user_from_id,
-        user_to_id: self.user_to_id
+     return {   
+        "user_from_id": self.user_from_id,
+        "user_to_id": self.user_to_id
      }
 
