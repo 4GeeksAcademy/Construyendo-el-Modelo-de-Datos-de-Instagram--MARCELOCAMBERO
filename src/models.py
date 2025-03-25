ccb01a580b0d0cdcd6786ef489c6b486a9f2d3b6
Marcelo Cambero <camberotje@gmail.com>
2025-03-25 16:12:24 +0000

    ejercicio hecho con javi

--- src/models.py
+++ src/models.py
@@ -1,19 +1,73 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Column, String, Integer, ForeignKey, Enum
+from sqlalchemy.orm import relationship
+import enum
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
-
+    __tablename__ = 'users'  # Cambié el nombre de la tabla para evitar conflictos
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), unique=True, nullable=False)
+    firstname = Column(String(50), nullable=False)
+    secondname = Column(String(50), nullable=False)
+    email = Column(String(50), unique=True, nullable=False)
+    comments = relationship('Comment', backref='user', lazy=True)
     def serialize(self):
         return {
             "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "secondname": self.secondname,
             "email": self.email,
-            # do not serialize the password, its a security breach
         }
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(50), nullable=False)
+    author_id = Column(Integer, nullable=False)
+    post_id = Column(Integer, nullable=False)
+
+    def serialize(self):
+        return {
+             "comment_text": self.comment_text,
+             "author_id": self.author_id,
+             "post_id": self.post_id,
+        }
+    
+class Post (db.Model):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, nullable=False)
+
+def serialize(self):
+        return {
+             "user_id": self.user_id,
+        }
+
+class Media (db.Model):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(Enum("image", name="media_types"))
+    url = Column(String)
+    post_id = Column(Integer, nullable=False)
+def serialize(self):
+        
+        return {
+            "id": self.id,
+            "type": self.type,
+            "url": self.url,
+            "post_id": self.post_id    
+        }
+
+class Follower (db.Model):
+     __tablename__ = 'media'
+     user_from_id = Column(Integer, primary_key=True)
+     user_to_id = Column(Integer)
+
+def serialize(self):
+     return {
+        user_from_id: self.user_from_id,
+        user_to_id: self.user_to_id
+     }
+
